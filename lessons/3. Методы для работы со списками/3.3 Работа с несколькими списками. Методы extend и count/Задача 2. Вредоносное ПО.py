417b825e7c13dccd1476d70f0d3d1edd1538697e
MVINFRVME <galgamet1337@yandex.ru>
2024-03-20 18:50:44 +0500

    Прошел урок 3.3 и решил задачи

---
+++ lessons/3. Методы для работы со списками/3.3 Работа с несколькими списками. Методы extend и count/Задача 2. Вредоносное ПО.py
@@ -0,0 +1,33 @@
+# Гера решил попрактиковаться в программировании и захотел написать небольшой скрипт, который после двух сообщений
+# отправляет ещё одно на основе первых двух.
+#
+# Пользователь вводит две строки. В каждой из них есть какое-то количество специальных символов ! и ?.
+# Напишите программу, которая считает количество этих символов отдельно в первой строке и отдельно во второй.
+# Если в первой строке их больше, чем во второй, то на экран выводится первая строчка, объединённая со второй,
+# а иначе — вторая с первой. При равном количестве символов в строках выводится «Ой».
+
+first_message = input('Первое сообщение: ')
+second_message = input('Второе сообщение: ')
+
+first_message_count = first_message.count('!') + first_message.count('?')
+second_message_count = second_message.count('!') + second_message.count('?')
+
+print('Третье сообщение: ', end='')
+
+if first_message_count > second_message_count:
+    print(f'{first_message}{second_message}')
+elif first_message_count < second_message_count:
+    print(f'{second_message}{first_message}')
+else:
+    print('Ой')
+
+# Решение учителя
+
+# first_word = input("Первое сообщение: ")
+# second_word = input("Первое сообщение: ")
+# first_count = first_word.count("!") + first_word.count("?")
+# second_count = second_word.count("!") + second_word.count("?")
+# if first_count < second_count:
+#     first_word, second_word = second_word, first_word  # пусть первым словом будет то, в котором больше знаков
+#
+# print(first_word + second_word)
---
+++ lessons/3. Методы для работы со списками/3.3 Работа с несколькими списками. Методы extend и count/Задача 3. Пакеты.py
@@ -0,0 +1,25 @@
+
+number_of_packs = int(input('Кол-во пакетов: '))
+final_pack_list = []
+total_lost_packs = 0
+
+for pack in range(number_of_packs):
+    print(f'\nПакет номер {pack + 1}')
+    pack_list = []
+
+    for i in range(4):
+        bit = int(input(f'{i + 1} бит: '))
+        pack_list.append(bit)
+
+    if pack_list.count(-1) <= 1:
+        final_pack_list.extend(pack_list)
+    else:
+        print('Много ошибок в пакете')
+        total_lost_packs += 1
+
+
+print(
+    f'\nПолученное сообщение: {final_pack_list}\n'
+    f'Кол-во ошибок в сообщении: {final_pack_list.count(-1)}\n'
+    f'Кол-во потерянных пакетов: {total_lost_packs}'
+)
