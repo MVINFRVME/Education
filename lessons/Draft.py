eb27eff01226865eb8e96eb977ae5ceaefe2b26d
MVINFRVME <galgamet1337@yandex.ru>
2024-04-26 20:39:29 +0500

    Закончил 8.4

--- lessons/Draft.py
+++ lessons/Draft.py
@@ -1,18 +1,21 @@
-s = "lotut"
-vowels = set('aeiou')
-res = list(s)
-i, j = 0, len(s) - 1
+import sys
+import copy
 
-while i < j:
-    if res[i] not in vowels:
-        i += 1
-    elif res[j] not in vowels:
-        j -= 1
-    else:
-        res[i], res[j] = res[j], res[i]
-        i += 1
-        j -= 1
+b = (1, 2, 3)
+a = [1, 2, 3]
+print("Размер списка a в памяти:", sys.getsizeof(a), "байт")
+print("Размер списка a в памяти:", sys.getsizeof(b), "байт")
 
-res_str = ''.join(res)
 
-print(res_str)
+ab = [1, [2, 4], 3]
+ac = ab.copy()
+print(ab)
+print(ac)
+ab[1][1] = 3
+print(ab)
+print(ac)
+
+print(id('2'))
+
+
+
