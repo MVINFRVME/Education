91256fc13c02c5d49fe2700113605a46a0ce683b
MVINFRVME <galgamet1337@yandex.ru>
2024-08-28 16:55:58 +0500

    Practice work module 14 added task 4.

--- lessons/Draft2.py
+++ lessons/Draft2.py
@@ -1,25 +1,20 @@
+
 class Solution:
-    def findComplement(self, num: int) -> int:
-        bin_str = str(format(num, 'b'))
-        new_num = ''
-        for i in bin_str:
-            if i == '0':
-                new_num += '1'
-            else:
-                new_num += '0'
+    def maxDepth(self, s: str) -> int:
+        count = 0
+        result = 0
+        for sym in s:
+            if count > result:
+                result = count
+            if sym == '(':
+                count += 1
+            elif sym == ')':
+                count -= 1
 
-        return int(new_num, 2)
-
-
+        return result
 
 
 ex = Solution()
-ex.findComplement(5)
-assert ex.findComplement(5) == 2
-assert ex.findComplement(5) == 2, "Test case 1 failed"
-assert ex.findComplement(1) == 0, "Test case 2 failed"
-assert ex.findComplement(2) == 1, "Test case 3 failed"
-assert ex.findComplement(3) == 0, "Test case 4 failed"
-assert ex.findComplement(4) == 3, "Test case 5 failed"
-assert ex.findComplement(30) == 1, "Test case 6 failed"  # 30 -> 11110 in binary, complement -> 00001
-assert ex.findComplement(15) == 0, "Test case 7 failed"
+assert ex.maxDepth("(1+(2*3)+((8)/4))+1") == 3
+assert ex.maxDepth("(1)+((2))+(((3)))") == 3
+assert ex.maxDepth("()(())((()()))") == 3
