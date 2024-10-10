75d0c1dd1fd64003726d063b5c0ff25e055ff566
MVINFRVME <galgamet1337@yandex.ru>
2024-10-10 17:30:32 +0500

    module 16 practice work done

--- lessons/Draft2.py
+++ lessons/Draft2.py
@@ -2,27 +2,26 @@ from typing import Callable, List
 
 
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        i, j = nums[0], nums[1]
-        if i < j:
-            i, j = nums[1], nums[0]
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        if len(s) == 0:
+            return 0
+        g.sort(), s.sort()
+        result = 0
+        for j in range(len(s)):
+            if s[j] >= g[0]:
+                start_index = j
+                break
+        else:
+            return 0
 
-        for num in nums[2:]:
-            if num > i and num > j:
-                j = i
-                i = num
-            elif num > i:
-                i = num
-            elif num > j:
-                j = num
-        return (i - 1) * (j - 1)
+        for i in range(start_index, len(g)):
+
+
 
 
 ex = Solution()
-assert ex.maxProduct(nums = [3,4,5,2]) == 12
-assert ex.maxProduct(nums = [4,3,5,2]) == 12
-assert ex.maxProduct(nums = [1,5,4,5]) == 16
-assert ex.maxProduct(nums = [3,7]) == 12
-assert ex.maxProduct(nums = [2,4,1,2,3,1,2,5,6,2,4,5,5,5,6,6,6,7,6,5,3,2,7]) == 36
-assert ex.maxProduct(nums = [2,4,1,2,3,1,2,5,6,2,4,5,5,5,6,6,6,7,6,5,3,2]) == 30
-assert ex.maxProduct(nums = [2,4,1,2,3,1,2,5,6,2,4,5,5,5,6,6,6,6,5,3,2]) == 25
+assert ex.findContentChildren(g = [5,4,3,3,2], s = []) == 0
+assert ex.findContentChildren(g = [5,4,3,3,2], s = [2,1]) == 1
+assert ex.findContentChildren(g = [1,2,3], s = [1,1]) == 1
+assert ex.findContentChildren(g = [1,2], s = [1,2,3]) == 2
+assert ex.findContentChildren(g = [2,4,3,3,2], s = [2,1,6,8]) == 3
--- lessons/Modules/module16/Задача 4. Весь мир декоратор....py
+++ lessons/Modules/module16/Задача 4. Весь мир декоратор....py
@@ -27,15 +27,31 @@
 # Есть аннотация типов для методов/функций и их аргументов (кроме args и kwargs). Если функция/метод ничего
 # не возвращают, то используется None.
 
+from typing import Callable
+import functools
+
+def decorator_with_args_for_any_decorator(decorator_to_enhance: Callable) -> Callable:
+    """Декоратор. Дает возможность другому декоратору принимать произвольные аргументы"""
+    def decorator_maker(*args, **kwargs) -> Callable:
+        def decorator_wrapper(func: Callable) -> Callable:
+            return decorator_to_enhance(func, *args, **kwargs)
+        return decorator_wrapper
+    return decorator_maker
+
 
 @decorator_with_args_for_any_decorator
-def decorated_decorator(func: Callable, *args, **kwargs):
+def decorated_decorator(func: Callable, *dec_args, **dec_kwargs):
+    """Декоратор. Шаблон"""
+    @functools.wraps(func)
+    def wrapper(*func_args, **func_kwargs) -> Callable:
+        print(f'Переданные арги и кварги в декоратор: {dec_args} {dec_kwargs}')
+        return func(*func_args, **func_kwargs)
+    return wrapper
+
 
 @decorated_decorator(100, 'рублей', 200, 'друзей')
 def decorated_function(text: str, num: int) -> None:
+    """Пример декорируемой функции"""
     print("Привет", text, num)
 
 decorated_function("Юзер", 101)
-# Результат:
-# Переданные арги и кварги в декоратор: (100, 'рублей', 200, 'друзей') {}
-# Привет, Юзер 101
