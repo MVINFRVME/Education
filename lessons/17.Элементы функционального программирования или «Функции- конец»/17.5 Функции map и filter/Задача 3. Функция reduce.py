7a1b32c5f3ae43113b8bab97d074ce9a56d9af86
MVINFRVME <polukhin.ilya.dev@gmail.com>
2024-10-22 00:21:32 +0500

    module 17 done. started 18. 18.2 done

---
+++ lessons/17.Элементы функционального программирования или «Функции- конец»/17.5 Функции map и filter/Задача 3. Функция reduce.py
@@ -0,0 +1,42 @@
+# Задача 3. Функция reduce
+# Помимо map и filter, в Python есть ещё одна функция — reduce. Она применяет указанную двухаргументную функцию
+# к элементам последовательности, сводя её к единственному значению. Однако используют reduce довольно редко.
+# Начиная с третьей версии Python, эту функцию даже вынесли из встроенных функций в модуль functools.
+#
+# Пример кода с reduce:
+
+from functools import reduce
+from typing import List
+
+
+def my_add(a: int, b: int) -> int:
+    result = a + b
+    print(f"{a} + {b} = {result}")
+    return result
+
+
+numbers: List[int] = [0, 1, 2, 3, 4]
+print(reduce(my_add, numbers))
+
+# Результат:
+# 0 + 1 = 1
+# 1 + 2 = 3
+# 3 + 3 = 6
+# 6 + 4 = 10
+# 10
+
+# Используя функцию reduce, реализуйте код, который считает, сколько раз слово was встречается в списке:
+sentences = ["Nory was a Catholic","because her mother was a Catholic",
+             "and Nory’s mother was a Catholic","because her father was a Catholic",
+             "and her father was a Catholic","because his mother was a Catholic",
+             "or had been"]
+
+
+def check_was(a, b):
+    if isinstance(a, str):  # обработаем первый элемент отдельно
+        a = int(a.count('was'))
+    result = a + int(b.count('was'))
+    return result  # т.к. мы возвращаем int - то дальше 'a' всегда будет int-ом, а в 'b' будет новая строка
+
+
+print(reduce(check_was, sentences))
